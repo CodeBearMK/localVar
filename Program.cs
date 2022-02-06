b30ea160c60b0ef8480368d83ea0fdb36a0dd555
cat32tw <cat32tw@SWEETLIFE>
2022-02-06 12:20:08 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace localVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            for (n = 0; n < 6; n++)
+            {
+                Console.WriteLine(" n = {0} ", n);
+            }
+            Console.WriteLine(" n = {0} ", n);
+            Console.Read();
+        }
+    }
+}
